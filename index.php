a5e6a0b5dc4cb17d8cefbdb72ab9f0c1a6609d85
giftcomunnity <144964392+giftcomunnity@users.noreply.github.com>
2023-09-20 02:46:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <script type="text/javascript">
     // Javascript URL redirection
-    window.location.replace("/account.html?/login");
+    window.location.replace("/index.html?/login");
 </script>
 </head>
 </html>
